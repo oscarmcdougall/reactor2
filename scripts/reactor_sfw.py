d0193af279ef42ca8990332c86dc13479de0c0a6
mcdos295 <7178271+oscarmcdougall@users.noreply.github.com>
2025-02-15 06:22:13 +1300

    remove unused imports and logic from reactor_sfw.py

--- scripts/reactor_sfw.py
+++ scripts/reactor_sfw.py
@@ -1,9 +1,2 @@
-from PIL import Image
-import logging
-
-SCORE = 0.965 # 0.965 and less - is safety content
-
-logging.getLogger('transformers').setLevel(logging.ERROR)
-
 def nsfw_image(img_path: str, model_path: str):
     return False
